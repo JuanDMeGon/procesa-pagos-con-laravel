91dd8b7e0922cbe0af460bf6c573c029325ac01c
JuanDMeGon <JuanDMeGon@users.noreply.github.com>
2019-09-18 01:03:54 -0500

    Agregando componentes para mostrar mensajes de la plataforma de pagos

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -77,6 +77,28 @@
         </nav>
 
         <main class="py-4">
+            <div class="container">
+                @if (isset($errors) && $errors->any())
+                    <div class="alert alert-danger">
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif
+
+                @if (session()->has('success'))
+                    <div class="alert alert-success">
+                        <ul>
+                            @foreach (session()->get('success') as $message)
+                                <li>{{ $message }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif
+            </div>
+
             @yield('content')
         </main>
     </div>
