9d0519507b1b7ba85261659ffde5aea774d6c185
JuanDMeGon <JuanDMeGon@users.noreply.github.com>
2019-09-18 18:48:16 -0500

    Configurando la plataforma de pagos en Laravel para usar la API de PayPal

--- config/services.php
+++ config/services.php
@@ -20,6 +20,12 @@ return [
         'endpoint' => env('MAILGUN_ENDPOINT', 'api.mailgun.net'),
     ],
 
+    'paypal' => [
+        'base_uri' => env('PAYPAL_BASE_URI'),
+        'client_id' => env('PAYPAL_CLIENT_ID'),
+        'client_secret' => env('PAYPAL_CLIENT_SECRET'),
+    ],
+
     'postmark' => [
         'token' => env('POSTMARK_TOKEN'),
     ],
