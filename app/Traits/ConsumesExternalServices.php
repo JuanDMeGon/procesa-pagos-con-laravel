49297d1bef5f47a869121261ff7d01d933b077b8
JuanDMeGon <JuanDMeGon@users.noreply.github.com>
2019-09-18 03:19:47 -0500

    Creando un componente para usar cualquier API de las plataformas de pagos

---
+++ app/Traits/ConsumesExternalServices.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Traits;
+
+use GuzzleHttp\Client;
+
+trait ConsumesExternalServices
+{
+    public function makeRequest($method, $requestUrl, $queryParams = [], $formParams = [], $headers = [], $isJsonRequest = false)
+    {
+        $client = new Client([
+            'base_uri' => $this->baseUri,
+        ]);
+
+        if (method_exists($this, 'resolveAuthorization')) {
+            $this->resolveAuthorization($queryParams, $formParams, $headers);
+        }
+
+        $response = $client->request($method, $requestUrl, [
+            $isJsonRequest ? 'json' : 'form_params' => $formParams,
+            'headers' => $headers,
+            'query' => $queryParams,
+        ]);
+
+        $response = $response->getBody()->getContents();
+
+        if (method_exists($this, 'decodeResponse')) {
+            $response = $this->decodeResponse($response);
+        }
+
+        return $response;
+    }
+}
