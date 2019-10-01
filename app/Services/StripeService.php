49349a57fe83b61badaac29179c88425c391b6e8
JuanDMeGon <JuanDMeGon@users.noreply.github.com>
2019-10-01 18:33:31 -0500

    Creando el servicio para Laravel que usará la API de Stripe

---
+++ app/Services/StripeService.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Http\Request;
+use App\Traits\ConsumesExternalServices;
+
+class StripeService
+{
+    use ConsumesExternalServices;
+
+    protected $key;
+
+    protected $secret;
+
+    protected $baseUri;
+
+    public function __construct()
+    {
+        $this->baseUri = config('services.stripe.base_uri');
+        $this->key = config('services.stripe.key');
+        $this->secret = config('services.stripe.secret');
+    }
+
+    public function resolveAuthorization(&$queryParams, &$formParams, &$headers)
+    {
+        //
+    }
+
+    public function decodeResponse($response)
+    {
+        return json_decode($response);
+    }
+
+    public function resolveAccessToken()
+    {
+        //
+    }
+
+    public function handlePayment(Request $request)
+    {
+        //
+    }
+
+    public function handleApproval()
+    {
+        //
+    }
+
+    public function resolveFactor($currency)
+    {
+        $zeroDecimalCurrencies = ['JPY'];
+
+        if (in_array(strtoupper($currency), $zeroDecimalCurrencies)) {
+            return 1;
+        }
+
+        return 100;
+    }
+}
