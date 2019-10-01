cc4cf2363c6a9df2c8541a1d6777931d0b572bdf
JuanDMeGon <JuanDMeGon@users.noreply.github.com>
2019-10-01 15:47:50 -0500

    Creando un componente que resuelva una plataforma de pago dinámicamente

---
+++ app/Resolvers/PaymentPlatformResolver.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Resolvers;
+
+use Exception;
+use App\PaymentPlatform;
+
+class PaymentPlatformResolver
+{
+    protected $paymentPlatforms;
+
+    public function __construct()
+    {
+        $this->paymentPlatforms = PaymentPlatform::all();
+    }
+
+    public function resolveService($paymentPlatformId)
+    {
+        $name = strtolower($this->paymentPlatforms->firstWhere('id', $paymentPlatformId)->name);
+
+        $service = config("services.{$name}.class");
+
+        if ($service) {
+            return resolve($service);
+        }
+
+        throw new Exception('The selected payment platform is not in the configuration');
+    }
+}
--- config/services.php
+++ config/services.php
@@ -24,6 +24,7 @@ return [
         'base_uri' => env('PAYPAL_BASE_URI'),
         'client_id' => env('PAYPAL_CLIENT_ID'),
         'client_secret' => env('PAYPAL_CLIENT_SECRET'),
+        'class' => App\Services\PayPalService::class,
     ],
 
     'postmark' => [
