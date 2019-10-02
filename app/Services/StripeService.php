85707857969425fe4e357d2c8d733f3bffdfa0e5
JuanDMeGon <JuanDMeGon@users.noreply.github.com>
2019-10-01 22:29:51 -0500

    Creando una intención de pago con la API de Stripe desde Laravel

--- app/Services/StripeService.php
+++ app/Services/StripeService.php
@@ -47,6 +47,21 @@ class StripeService
         //
     }
 
+    public function createIntent($value, $currency, $paymentMethod)
+    {
+        return $this->makeRequest(
+            'POST',
+            '/v1/payment_intents',
+            [],
+            [
+                'amount' => round($value * $this->resolveFactor($currency)),
+                'currency' => strtolower($currency),
+                'payment_method' => $paymentMethod,
+                'confirmation_method' => 'manual',
+            ],
+        );
+    }
+
     public function resolveFactor($currency)
     {
         $zeroDecimalCurrencies = ['JPY'];
