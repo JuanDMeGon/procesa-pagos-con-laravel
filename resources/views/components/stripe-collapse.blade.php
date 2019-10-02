2e848a695f1357f9f6124f080965eb3a8c0b8d71
JuanDMeGon <JuanDMeGon@users.noreply.github.com>
2019-10-01 20:56:46 -0500

    Solicitando un método de pago para hacer cobros con Stripe desde Laravel

--- resources/views/components/stripe-collapse.blade.php
+++ resources/views/components/stripe-collapse.blade.php
@@ -1,3 +1,52 @@
-<small>You will be redirected to the Stripe checkout page</small>
-<input type="text" name="test">
-<input type="text" name="test2">
+@push('styles')
+<style type="text/css">
+    /**
+     * The CSS shown here will not be introduced in the Quickstart guide, but shows
+     * how you can use CSS to style your Element's container.
+     */
+    .StripeElement {
+      box-sizing: border-box;
+
+      height: 40px;
+
+      padding: 10px 12px;
+
+      border: 1px solid transparent;
+      border-radius: 4px;
+      background-color: white;
+
+      box-shadow: 0 1px 3px 0 #e6ebf1;
+      -webkit-transition: box-shadow 150ms ease;
+      transition: box-shadow 150ms ease;
+    }
+
+    .StripeElement--focus {
+      box-shadow: 0 1px 3px 0 #cfd7df;
+    }
+
+    .StripeElement--invalid {
+      border-color: #fa755a;
+    }
+
+    .StripeElement--webkit-autofill {
+      background-color: #fefde5 !important;
+    }
+</style>
+@endpush
+
+
+<label class="mt-3">Card details:</label>
+<div id="cardElement"></div>
+<small class="form-text text-muted" id="cardErrors" role="alert"></small>
+
+@push('scripts')
+<script src="https://js.stripe.com/v3/"></script>
+<script>
+    const stripe = Stripe('{{ config('services.stripe.key') }}');
+
+    const elements = stripe.elements({locale: 'en'});
+    const cardElement = elements.create('card');
+
+    cardElement.mount('#cardElement');
+</script>
+@endpush
