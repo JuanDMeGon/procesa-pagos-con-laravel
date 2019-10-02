7fccd1cda85d27f8149b20ed309fb8cc480fec39
JuanDMeGon <JuanDMeGon@users.noreply.github.com>
2019-10-01 23:50:57 -0500

    Realizando la validación 3D Secure directamente con Stripe

--- resources/views/stripe/3d-secure.blade.php
+++ resources/views/stripe/3d-secure.blade.php
@@ -14,4 +14,22 @@
         </div>
     </div>
 </div>
+
+@push('scripts')
+<script src="https://js.stripe.com/v3/"></script>
+
+<script>
+    const stripe = Stripe('{{ config('services.stripe.key') }}');
+
+    stripe.handleCardAction("{{ $clientSecret }}")
+        .then(function (result) {
+            if (result.error) {
+                window.location.replace("{{ route('cancelled') }}");
+            } else {
+                window.location.replace("{{ route('approval') }}");
+            }
+        });
+
+</script>
+@endpush
 @endsection
