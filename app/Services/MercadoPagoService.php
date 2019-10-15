f835fba0c6d4d4cacb8f89f1c9574b0ba8d404c3
JuanDMeGon <JuanDMeGon@users.noreply.github.com>
2019-10-15 16:47:42 -0500

    Creando un servicio en Laravel para usar la API de MercadoPago

---
+++ app/Services/MercadoPagoService.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Http\Request;
+use App\Traits\ConsumesExternalServices;
+
+class MercadoPagoService
+{
+    use ConsumesExternalServices;
+
+    protected $baseUri;
+
+    protected $key;
+
+    protected $secret;
+
+    protected $baseCurrency;
+
+    public function __construct()
+    {
+        $this->baseUri = config('services.mercadopago.base_uri');
+        $this->key = config('services.mercadopago.key');
+        $this->secret = config('services.mercadopago.secret');
+        $this->baseCurrency = config('services.mercadopago.base_currency');
+    }
+
+    public function resolveAuthorization(&$queryParams, &$formParams, &$headers)
+    {
+        //
+    }
+
+    public function decodeResponse($response)
+    {
+        return json_decode($response);
+    }
+
+    public function resolveAccessToken()
+    {
+        //
+    }
+
+    public function handlePayment(Request $request)
+    {
+        //
+    }
+
+    public function handleApproval()
+    {
+        //
+    }
+
+    public function resolveFactor($currency)
+    {
+        //
+    }
+}
