aa023bcb9df17da20d6a38200e332799c3044d6f
JuanDMeGon <JuanDMeGon@users.noreply.github.com>
2019-10-15 15:32:58 -0500

    Convirtiendo monedas con el servicio agregado a Laravel

--- app/Services/CurrencyConversionService.php
+++ app/Services/CurrencyConversionService.php
@@ -35,7 +35,16 @@ class CurrencyConversionService
 
     public function convertCurrency($from, $to)
     {
-        //
+        $response = $this->makeRequest(
+            'GET',
+            '/api/v7/convert',
+            [
+                'q' => "{$from}_{$to}",
+                'compact' => 'ultra',
+            ],
+        );
+
+        return $response->{strtoupper("{$from}_{$to}")};
     }
 
 }
