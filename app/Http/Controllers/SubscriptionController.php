45f3aaa1cbe6c1a00538bcb570ab7cecf67f7636
JuanDMeGon <JuanDMeGon@users.noreply.github.com>
2021-02-13 19:41:27 -0500

    Crea una suscripción en cuanto sea procesada con éxito

--- app/Http/Controllers/SubscriptionController.php
+++ app/Http/Controllers/SubscriptionController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\PaymentPlatform;
 use App\Plan;
 use App\Resolvers\PaymentPlatformResolver;
+use App\Subscription;
 use Illuminate\Http\Request;
 
 class SubscriptionController extends Controller
@@ -45,13 +46,33 @@ class SubscriptionController extends Controller
         return $paymentPlatform->handleSubscription($request);
     }
 
-    public function approval()
+    public function approval(Request $request)
     {
+        $rules = [
+            'plan' => ['required', 'exists:plans,slug']
+        ];
+
+        $request->validate($rules);
+
+        $plan = Plan::where('slug', $request->plan)->firstOrFail();
+        $user = $request->user();
+
+        $subscription = Subscription::create([
+            'active_until' => now()->addDays($plan->duration_in_days),
+            'user_id' => $user->id,
+            'plan_id' => $plan->id,
+        ]);
+
+        return redirect()
+            ->route('home')
+            ->withSuccess(['payment' => "Thanks, {$user->name}. You have a {$plan->slug} subscription. Start using it."]);
 
     }
 
     public function cancelled()
     {
-
+        return redirect()
+            ->route('subscribe.show')
+            ->withErrors('You cancelled. Comeback whenever you\'re ready :)');
     }
 }
